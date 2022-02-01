cacc3c575c93ae1706aca9d3c4c172134bc488c4
Aykut Vuruşkaner <AYKUT.VURUSKANER@AYKUTVNSB>
2022-02-01 13:05:03 +0300

    .

--- src/Lib.Localization/Dto/LanguageDto.cs
+++ src/Lib.Localization/Dto/LanguageDto.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Lib.Localization.Dto
 {
     [DapperORM.Attributes.Table("LibLanguage")]
